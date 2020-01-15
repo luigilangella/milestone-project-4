98c79009d160d4082967eaafb2c368d169183b17
Luigi Langella <luigi76langella@gmail.com>
2020-01-15 20:55:58 +0000

    Add cart and stripe functionality to take card payments

--- food_store/urls.py
+++ food_store/urls.py
@@ -21,6 +21,7 @@ from search import urls as urls_search
 from products import urls as urls_products
 from products.views import all_products
 from cart import urls as urls_cart
+from checkout import urls as urls_checkout
 from post import urls as urls_post
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -31,6 +32,7 @@ urlpatterns = [
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^checkout/',include(urls_checkout)),
     url(r'^posts/', include(urls_post)),
     url(r'^search/',include(urls_search)),
     url(r'^media(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
